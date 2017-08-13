30b2e90a7ed61f1994724a41df5c690ffc3f39ba
brett little <blittle@primehealthcare.com>
2017-08-12 17:34:39 -0700

    removed unfinished gesture detector

--- app/src/main/java/com/wingitgames/neurealitytechdemo/neurealitytechdemo/MainActivity.java
+++ app/src/main/java/com/wingitgames/neurealitytechdemo/neurealitytechdemo/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
@@ -18,8 +17,6 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    GestureDetector gestureDetector;
 
     private static final int MAX_CLICK_DURATION = 100;
     private long startClickTime;
@@ -129,7 +126,6 @@ public class MainActivity extends AppCompatActivity {
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        gestureDetector = new GestureDetector(getApplicationContext(), new GestureListener());
 
         getWindow().getDecorView().setSystemUiVisibility(
                           View.SYSTEM_UI_FLAG_LAYOUT_STABLE
